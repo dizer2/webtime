ae91c799c5028af68f1bb110ef20b56a0436c546
Yurii <fanfjrfj589@gmail.com>
2024-01-26 10:14:32 +0100

    fix small bug

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { Suspense, useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './i18n.jsx';
+import App from './App.jsx';
+import './i18n.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Suspense fallback={<p>Loading...</p>}>
-      <LoadingDelay delay={1000}>
+      {/* <LoadingDelay delay={1000}> */}
         <App />
-      </LoadingDelay>
+      {/* </LoadingDelay> */}
     </Suspense>
   </React.StrictMode>
 );
