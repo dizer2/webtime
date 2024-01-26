2e5581f981a27ffbeda5a901898b039eb0f46313
Roman Artemiev <115183967+Roman-Artemiev@users.noreply.github.com>
2024-01-26 09:56:58 +0100

    Add logic for localisation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import React, { Component, Suspense } from 'react';
+import { useTranslation, withTranslation, Trans } from 'react-i18next';
+import Home from './components/Home/Home';
+
+
+// use hoc for class based components
+class LegacyWelcomeClass extends Component {
+  render() {
+    const { t } = this.props;
+    return <h2>{t('title')}</h2>;
+  }
+}
+const Welcome = withTranslation()(LegacyWelcomeClass);
+
 
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <div className="App">
-      
+      <Home/>
     </div>
   );
 }
+
+
 
 export default App;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { Component, Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Home = () => {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+};
+
+
   return (
-	<div>
-	  
-	</div>
+	<section className='home'>
+      <h2>{t('title')}</h2>
+
+      <button onClick={() => changeLanguage('ua')}>UA</button> 
+      <button onClick={() => changeLanguage('cz')}>CZ</button> 
+	</section>
   )
 }
 
