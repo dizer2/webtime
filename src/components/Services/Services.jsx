d5c202804b5b16e0bed593341dd98a83621a5f61
Roman Artemiev <115183967+Roman-Artemiev@users.noreply.github.com>
2024-03-19 08:13:20 +0100

    Add two services cards

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -12,6 +12,8 @@ import PosterIcon from "./../main/img/poster-icon.svg";
 import MobileIcon from "./../main/img/phone-icon.svg";
 import CardIcon from "./../main/img/card-icon.svg";
 import PhoneIcon from "./../Services/img/phone-icon.svg";
+import FullWebsite from "./../main/img/complete-website-creation.svg";
+import OptimisationIcon from "./../main/img/optimisation-icon.svg";
 import { Reveal } from '../utils/Reveal/Reveal.tsx'
 import { RevealSecodary } from '../utils/RevealSecodary/RevealSecodary.tsx'
 import { Link } from 'react-router-dom'
@@ -50,7 +52,7 @@ const Services = () => {
                                 top = {50}
                             />
 
-                            <h3 className="services__card-title">{t('homePage.main.services.service1')}</h3>    
+                            <h3 className="services__card-title">{t('homePage.services.cardTitle1')}</h3>    
                             <p className="services__card-description">{t('homePage.services.card1Description')}</p>      
                         </div>
 
@@ -77,7 +79,7 @@ const Services = () => {
                                 top = {50}
                             />
 
-                            <h3 className="services__card-title">{t('homePage.main.services.service2')}</h3>    
+                            <h3 className="services__card-title">{t('homePage.services.cardTitle2')}</h3>    
                             <p className="services__card-description">{t('homePage.services.card2Description')}</p>      
                         </div>
 
@@ -106,7 +108,7 @@ const Services = () => {
                                 top = {50}
                             />
 
-                            <h3 className="services__card-title">{t('homePage.main.services.service3')}</h3>    
+                            <h3 className="services__card-title">{t('homePage.services.cardTitle3')}</h3>    
                             <p className="services__card-description">{t('homePage.services.card3Description')}</p>      
                         </div>
 
@@ -136,7 +138,7 @@ const Services = () => {
                                 top = {50}
                             />
 
-                            <h3 className="services__card-title">{t('homePage.main.services.service4')}</h3>    
+                            <h3 className="services__card-title">{t('homePage.services.cardTitle4')}</h3>    
                             <p className="services__card-description">{t('homePage.services.card4Description')}</p>      
                         </div>
 
@@ -165,7 +167,7 @@ const Services = () => {
                                 top = {50}
                             />
 
-                            <h3 className="services__card-title">{t('homePage.main.services.service5')}</h3>    
+                            <h3 className="services__card-title">{t('homePage.services.cardTitle5')}</h3>    
                             <p className="services__card-description">{t('homePage.services.card5Description')}</p>      
                         </div>
 
@@ -181,6 +183,58 @@ const Services = () => {
                 </Link>
             </RevealSecodary>
 
+            <RevealSecodary>
+                <div className="services__card">
+                    <div className="services__card-main">
+                        <IconBox 
+                            imgPath={FullWebsite}
+                            size = "medium"
+
+                            circleSize = {26}
+                            top = {50}
+                        />
+
+                        <h3 className="services__card-title">{t('homePage.services.cardTitle6')}</h3>    
+                        <p className="services__card-description">{t('homePage.services.card6Description')}</p>      
+                    </div>
+
+                        <ArrowBtn
+                            text = {t('homePage.main.viewMore')}
+                            customGradient={{
+                                direction: '99deg',
+                                colors: ['#2BBFFE', '#8629FD', '#FC4AF5'],
+                            }}
+                            textColor={arrowBtnColor}
+                        />    
+                </div>
+            </RevealSecodary>
+
+            <RevealSecodary>
+                <div className="services__card">
+                    <div className="services__card-main">
+                        <IconBox 
+                            imgPath={OptimisationIcon}
+                            size = "medium"
+
+                            circleSize = {26}
+                            top = {50}
+                        />
+
+                        <h3 className="services__card-title">{t('homePage.services.cardTitle7')}</h3>    
+                        <p className="services__card-description">{t('homePage.services.card7Description')}</p>      
+                    </div>
+
+                        <ArrowBtn
+                            text = {t('homePage.main.viewMore')}
+                            customGradient={{
+                                direction: '99deg',
+                                colors: ['#2BBFFE', '#8629FD', '#FC4AF5'],
+                            }}
+                            textColor={arrowBtnColor}
+                        />    
+                </div>
+            </RevealSecodary>
+
 		    <RevealSecodary>
                 <div className="services__card services__card--contact">
                     <div className="services__card-main">
@@ -192,8 +246,8 @@ const Services = () => {
                             top = {50}
                         />
 
-                        <h3 className="services__card-title">{t('homePage.main.services.service6')}</h3>    
-                        <p className="services__card-description">{t('homePage.services.card6Description')}</p>      
+                        <h3 className="services__card-title">{t('homePage.services.cardTitle8')}</h3>    
+                        <p className="services__card-description">{t('homePage.services.card8Description')}</p>      
                     </div>
 
                     <ArrowBtn
